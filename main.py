c7c9933cfe18b432cf325e662f079a2a534f4181
Даниил Пушкарев <d.pushkarev@decosystems.ru>
2023-12-26 10:43:23 +0300

    Pre-completed task

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ def create_exc_func(func: str) -> callable:
 
 
 def get_intersection(A: Point, B: Point, l: float) -> Point:
-    m = ((A.y - B.y) - l * (A.x - B.x)) / (2 * l)
-    return Point(m + A.x, A.y - m * l)
+    # Точка пересечения
+    x = (A.y - B.y) / (2 * l) + (A.x + B.x) / 2
+
+    # Значение ломанной в данной точке
+    y = B.y + l * (x - B.x)
+    return Point(x, y)
 
 
-def shubert_piyavskii(func: callable, a: float, b: float, l: float, epsilon=0.001):
+def shubert_piyavskii(func: callable, a: float, b: float, l: float, epsilon=0.001) -> float:
     plt.plot(dots, list(map(func, dots)))
     plt.grid()
 
@@ -34,37 +38,46 @@ def shubert_piyavskii(func: callable, a: float, b: float, l: float, epsilon=0.00
     plt.plot(dots, list(map(p_1, dots)))
 
     u = get_intersection(A, B, l)
-    left_u, right_u = get_intersection(A, u, l), get_intersection(u, B, l)
-    points = [A, left_u, u, right_u, B]
 
-    plt.scatter([point.x for point in points], [point.y for point in points])
+    line_points = [Point(A.x, p_1(A.x)), u, Point(B.x, p_1(B.x))]
+    points = [A, Point(u.x, func(u.x)), B]
 
     delta = 10 ** 10_000
     while delta > epsilon:
-        min_point_idx = np.argmin([point.y for point in points])
+        # Индекс минимума ломаной
+        min_line_point_idx = np.argmin([point.y for point in line_points])
 
-        min_point = Point(points[min_point_idx].x, func(points[min_point_idx].x))
-        delta = min_point.y - points[min_point_idx].y
+        # Получение значений на ломаной и функции
+        min_line_point = line_points[min_line_point_idx]
+        min_point = points[min_line_point_idx]
 
-        left_point = get_intersection(points[min_point_idx - 1], min_point, l)
-        right_point = get_intersection(min_point, points[min_point_idx + 1], l)
+        delta = min_point.y - min_line_point.y
 
-        points.pop(min_point_idx)
+        left_point = get_intersection(points[min_line_point_idx - 1], min_point, l)
+        right_point = get_intersection(min_point, points[min_line_point_idx + 1], l)
 
-        points.insert(min_point_idx, left_point)
+        func_left_point = Point(left_point.x, func(left_point.x))
+        func_right_point = Point(right_point.x, func(right_point.x))
 
-        points.insert(min_point_idx, min_point)
+        line_points[min_line_point_idx] = min_point
+        line_points.insert(min_line_point_idx, left_point)
+        line_points.insert(min_line_point_idx + 2, right_point)
 
-        points.insert(min_point_idx, right_point)
+        points.insert(min_line_point_idx, func_left_point)
+        points.insert(min_line_point_idx + 2, func_right_point)
+
+        plt.plot([left_point.x, min_point.x, right_point.x], [left_point.y, min_point.y, right_point.y])
 
     plt.show()
 
+    return min_point
 
 a, b = -1.5, 1.5
 dots = np.linspace(a, b, 1000)
 
 t = '(1.5 - x + x * 0.5)^2 + (2.25 - x + x*0.5^2)^2 + (2.625 - x + x * 0.5^3)^2'
 func = '(x^2-1)^2+x'
+
+
 func = create_exc_func(func)
-# print(piyavskii_method(func, a, b, 5, 0.001))
 shubert_piyavskii(func, a, b, 5)
