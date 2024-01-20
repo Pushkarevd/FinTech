a83304fe764f3648e929a679dc008bafb0447f46
pushkarevd <pushkarevd.89@gmail.com>
2024-01-20 14:10:13 +0300

    Finished Task-9

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-
-# from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
 
 
 class ExpertTask:
@@ -39,15 +40,46 @@ class ExpertTask:
             index=[i + 1 for i in range(C_matrix.shape[1])],
         )
 
+    def __kendall(self):
+        m = self._matrix.shape[1]
+        n = self._matrix.shape[0]
+
+        sums = [sum(x) for x in self._matrix]
+        Rbar = sum(sums) / n
+        S = sum([(sums[x] - Rbar) ** 2 for x in range(n)])
+
+        W = (12 * S) / (m ** 2 * (n ** 3 - n))
+
+        return W
+
     def __write_result(self):
         with open("output.txt", "w") as file:
             file.write(
                 f'Вектор оценки экспертов: {self.__get_objects_estimation().values}\nВектор квалификации экспертов: {self.__get_expert_qualification().values}'
             )
 
+    def __plot_dots(self):
+        pca = PCA(n_components=2)
+
+        pca.fit(self._matrix)
+        X_pca = pca.transform(self._matrix)
+        clusters = self.__cluster()
+
+        plt.title('Визуализация с методом PCA и кластеризации DBSCAN')
+        plt.scatter(X_pca[:, 0], X_pca[:, 1], c=clusters)
+        plt.grid()
+        plt.show()
+
+    def __cluster(self):
+        clf = DBSCAN(min_samples=2, eps=2)
+        clusters = clf.fit_predict(self._matrix)
+        return clusters
+
     def start(self):
         print(self.matrix)
         self.__write_result()
+        print(f'Kendall W - {self.__kendall()}')
+        self.__plot_dots()
 
 
 if __name__ == "__main__":
